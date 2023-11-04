b8baf50eb964cd39bf2e469791d5efae24833ad4
AdityaR10 <118066796+AdityaR10@users.noreply.github.com>
2023-11-04 22:35:42 +0530

    acha

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
+app.all('/:name', async (req, res) => {
+    const data = await (await fetch(`https://www.instagram.com/${name}/?__a=1&__d=1`)).json();
+    res.json(data);
 })
 app.listen(process.env.PORT || 3000)
